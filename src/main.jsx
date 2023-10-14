20f346b65106c8d5c6427b0fee45029abcc9bcef
luthfiahmdf <luthfiahmdf@gmail.com>
2023-10-14 20:47:21 +0700

    feature:add-tailwind-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home, Programing } from "./pages";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/programing",
+    element: <Programing />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
---
+++ src/pages/programing/index.jsx
@@ -0,0 +1,3 @@
+export const Programing = () => {
+  return <div className="text-emerald-200">Programming</div>;
+};
