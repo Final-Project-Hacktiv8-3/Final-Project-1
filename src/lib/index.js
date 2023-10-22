9d18d02805ff9daa323572a395d563dd4b692927
shidqi29 <shidqiamirulhaq@gmail.com>
2023-10-22 20:59:06 +0700

    fix: style news card & template main content

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,14 @@ export const endpoints = {
   programing: "everything?q=programing",
   covid19: "everything?q=covid-19",
 };
+
+export const formattedDate = (input) => {
+  const date = new Date(input);
+  const formatted = date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+  if (formatted === "Invalid Date") return "-";
+  return formatted;
+};
